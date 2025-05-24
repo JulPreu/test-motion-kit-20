af8b293cb2c8a534174f2194a27b585fea71141f
JulPreu <julian.preuschoff@gmx.net>
2025-05-24 08:13:43 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-if (input.buttonIsPressed(Button.A)) {
-    maqueen.servoRun(maqueen.Servos.S1, 120)
-    basic.pause(2000)
-    maqueen.servoRun(maqueen.Servos.S1, 60)
-    basic.pause(2000)
-    maqueen.servoRun(maqueen.Servos.S1, 90)
-}
+maqueen.servoRun(maqueen.Servos.S1, 120)
+basic.pause(2000)
+maqueen.servoRun(maqueen.Servos.S1, 60)
+basic.pause(2000)
+maqueen.servoRun(maqueen.Servos.S1, 90)
 basic.forever(function () {
-    if (maqueen.ultrasonic(maqueen.DistanceUnit.Centimeters) < 20) {
-        maqueen.setColor(0xff0000)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-    } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
-        maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOn)
-        maqueen.setColor(0x00ffff)
+    if (input.buttonIsPressed(Button.A)) {
+        if (maqueen.ultrasonic(maqueen.DistanceUnit.Centimeters) < 20) {
+            maqueen.setColor(0xff0000)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        } else {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+            maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOn)
+            maqueen.setColor(0x00ffff)
+        }
     }
 })
