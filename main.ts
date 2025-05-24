a728210b4b367441ccd6787f2d3478714dea932f
JulPreu <julian.preuschoff@gmx.net>
2025-05-24 08:33:28 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,10 +4,10 @@ basic.pause(2000)
 maqueen.servoRun(maqueen.Servos.S1, 60)
 basic.pause(2000)
 maqueen.servoRun(maqueen.Servos.S1, 90)
-if (input.buttonIsPressed(Button.A)) {
-    start = 1
-}
 basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        start = 1
+    }
     if (start == 1) {
         if (maqueen.ultrasonic(maqueen.DistanceUnit.Centimeters) < 20) {
             maqueen.setColor(0xff0000)
